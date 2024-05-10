d0971b62fa5a9ed458de1a9d795fca9e1cd10e8e
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-05-10 16:03:44 +0600

    login and register error message showed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -67,17 +67,17 @@ const Login = () => {
                                 <div className="form-control">
                                 <label className="input flex items-center gap-1 mb-6">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="text-[#A75D5D] w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                                    <input {...register("email", { required: true })} type="email" placeholder="Enter Email Address" className="input w-full border-0 hover:border-0 focus:border-0" required />
-                                    {errors.email && <span>This field is required</span>}
+                                    <input {...register("email", { required: true })} type="email" placeholder="Enter Email Address" className="input w-full border-0 hover:border-0 focus:border-0" />
                                 </label>
+                                    {errors.email && <span className="text-red-500">This field is required</span>}
                                 
                                 </div>
                                 <div className="form-control">
                                 <label className="input flex items-center gap-1 mb-6">
                                     <RiLockPasswordFill className="text-[#A75D5D]"></RiLockPasswordFill>
-                                    <input {...register("password", { required: true })} type="password" placeholder="Password" className="input border-0 w-full hover:border-0 focus:border-0" required />
-                                    {errors.email && <span>This field is required</span>}
+                                    <input {...register("password", { required: true })} type="password" placeholder="Password" className="input border-0 w-full hover:border-0 focus:border-0"  />
                                 </label>
+                                    {errors.password && <span className="text-red-500">This field is required</span>}
                                 </div>
                                 <div className="form-control mt-6">
                                         <button className="btn text-white bg-[#F0997D]">Login</button>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -56,30 +56,30 @@ const Register = () => {
                     <div className="hero min-h-screen poppins">
                         <div className="hero-content flex-col lg:flex-row-reverse gap-6">
                             <div className="text-center lg:text-left flex-1">
-                                <img src="/src/assets/foodlogin.jpg" alt="" />
+                                <img src="/src/assets/foodregister.jpg" alt="" />
                             </div>
                             <div className="card shrink-0 w-full flex-1 shadow-2xl bg-base-100  p-4">
                             <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                                 <div className="form-control">
                                     <label className="input flex items-center gap-1 mb-6">
                                         <FaUser className="text-[#A75D5D]"></FaUser>
-                                        <input {...register("name", { required: true })} type="text" placeholder="Enter Your Name" className="input border-0 hover:border-0 focus:border-0 w-full" required />
-                                        {errors.name && <span>This field is required</span>}
+                                        <input {...register("name", { required: true })} type="text" placeholder="Enter Your Name" className="input border-0 hover:border-0 focus:border-0 w-full" />
                                     </label>
+                                        {errors.name && <span className="text-red-500">This field is required</span>}
                                 </div>
                                 <div className="form-control">
                                     <label className="input flex items-center gap-1 mb-6">
                                         <FaImage className="text-[#A75D5D]"></FaImage>
-                                        <input {...register("photoURL", { required: true })} type="text" placeholder="Enter Photo URL" className="input border-0 hover:border-0 focus:border-0 w-full" required />
-                                        {errors.photoURL && <span>This field is required</span>}
+                                        <input {...register("photoURL", { required: true })} type="text" placeholder="Enter Photo URL" className="input border-0 hover:border-0 focus:border-0 w-full"  />
                                     </label>
+                                        {errors.photoURL && <span className="text-red-500">This field is required</span>}
                                 </div>
                                 <div className="form-control">
                                     <label className="input flex items-center gap-1 mb-6">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="text-[#A75D5D] w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                                        <input {...register("email", { required: true })} type="email" placeholder="Enter Email Address" className="input border-0 w-full hover:border-0 focus:border-0" required />
-                                        {errors.email && <span>This field is required</span>}
+                                        <input {...register("email", { required: true })} type="email" placeholder="Enter Email Address" className="input border-0 w-full hover:border-0 focus:border-0" />
                                     </label>
+                                        {errors.email && <span className="text-red-500">This field is required</span>}
                                 </div>
                                 <div className="form-controls ">
                                     <label className="input flex items-center gap-1 relative mb-6 ">
@@ -90,8 +90,8 @@ const Register = () => {
                                             passwordState ? <FaRegEye className="text-xl"/> : <FaRegEyeSlash className="text-xl" />
                                         }
                                         </div>
-                                        {errors.email && <span>This field is required</span>}
                                     </label>
+                                        {errors.password && <span className="text-red-500">This field is required</span>}
                                 </div>
                                 <div className="form-control mt-6">
                                         <button className="btn text-white bg-[#F0997D]">Register</button>
