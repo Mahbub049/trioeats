88a8e658d10e7deebc60d8b4e0483fdd07a154b1
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-05-14 01:07:10 +0600

    card style changed in the all foods

--- src/components/AllFoods/AllFoods.jsx
+++ src/components/AllFoods/AllFoods.jsx
@@ -47,7 +47,7 @@ const AllFoods = () => {
                     </form>
                     </div>
                 </div>
-                <div className="mx-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+                <div className="mx-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {
                     foods.map(food=><FoodCard key={food._id} food={food}></FoodCard>)
                 }
--- src/components/AllFoods/FoodCard/FoodCard.jsx
+++ src/components/AllFoods/FoodCard/FoodCard.jsx
@@ -7,7 +7,33 @@ const FoodCard = ({food}) => {
     const {_id, foodname, category, image, price, quantity} = food;
     return (
         <div>
-            <div className="card lg:card-side bg-base-100 h-[312px] shadow-xl px-4 py-0 border-2 border-[#a75d5d29] poppins">
+            <div className="card bg-base-100 shadow-xl">
+            <figure className="flex-1"><img src={image} className="rounded-xl h-[323px] w-full" alt="Album"/></figure>
+            <div className="card-body px-2">
+                <h2 className="card-title font-bold text-2xl text-[#D3756B]">{foodname}</h2>
+                <div className="flex items-center gap-3 mt-2">
+                    <div className="flex gap-2 items-center">
+                        <BiSolidCategory className="text-xl"></BiSolidCategory>
+                        <h4 className="text-md"><span className="font-semibold">Category: </span>{category}</h4>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <FaSortAmountUpAlt className="text-xl"></FaSortAmountUpAlt>
+                        <p><span className="font-semibold">Quantity: </span>{quantity}</p>
+                    </div>
+                </div>
+                <div className="flex gap-2 items-center mt-4">
+                        <RiMoneyDollarBoxFill className="text-xl"></RiMoneyDollarBoxFill>
+                        <p className="font-semibold">Price:</p>
+                    </div>
+                        <p className="text-2xl font-extrabold text-[#A75D5D]">${price}</p>
+                <div className="card-actions justify-end mt-2">
+                        <Link to={`/fooddetails/${_id}`} className="btn w-full text-white bg-[#A75D5D]">Details</Link>
+                </div>
+
+            </div>
+            </div>
+
+            {/* <div className="card lg:card-side bg-base-100 h-[312px] shadow-xl px-4 py-0 border-2 border-[#a75d5d29] poppins">
                 <figure className="flex-1"><img src={image} className="rounded-xl w-full" alt="Album"/></figure>
                 <div className="card-body flex-1">
                     <h2 className="card-title font-bold text-2xl text-[#D3756B]">{foodname}</h2>
@@ -19,7 +45,7 @@ const FoodCard = ({food}) => {
                         <div className="flex gap-2 items-center">
                             <FaSortAmountUpAlt className="text-xl"></FaSortAmountUpAlt>
                             <p><span className="font-semibold">Quantity: </span>{quantity}</p>
-                    </div>
+                        </div>
                     </div>
                     <div className="flex gap-2 items-center mt-4">
                         <RiMoneyDollarBoxFill className="text-xl"></RiMoneyDollarBoxFill>
@@ -30,7 +56,7 @@ const FoodCard = ({food}) => {
                         <Link to={`/fooddetails/${_id}`} className="btn w-full text-white bg-[#A75D5D]">Details</Link>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
